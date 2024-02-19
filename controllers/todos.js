54db9291a01e72caa27bfec493b4b8c7027cc416
Sonia-Baibou <s.baibou@ohmycode.io>
2024-02-19 10:16:30 +0100

    remove login setHeader

--- controllers/todos.js
+++ controllers/todos.js
@@ -87,18 +87,18 @@ const login = async (req, res) => {
   console.log('test', req.body);
   const { email, pwd } = req.body;
   console.log(email, pwd);
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  // // another common pattern
+  // // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  // res.setHeader(
+  //   'Access-Control-Allow-Methods',
+  //   'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  // );
+  // res.setHeader(
+  //   'Access-Control-Allow-Headers',
+  //   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  // );
 
   try {
     // find the user
